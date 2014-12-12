00bb73fd79666181a56978104c45bb3947d975f6
xyz <xyzhtml@163.com>
2014-12-12 15:47:31 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+var path = require('path');
+var rd = require('rd');
 var layerproxy = require("./lib/layerproxy")
+
+//var dirs = DEFAULT_LAYER
+
+rd.eachFileFilterSync('models', /\.js$/, function (file) {
+    require(file)()
+});
+
 var larkMVC = function(option){
-  return function*(next) {
-      this.pageServices = layerproxy.pageServices
-      yield next
-  };
+    return function*(next) {
+        this.pageServices = layerproxy.pageServices
+            yield next
+    };
 }
 module.export = larkMVC
